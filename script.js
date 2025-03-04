18069209efa14731a47e1e5523cd5d35f04cdd17
Rakesh Sardar <rakeshsardar9030@gmail.com>
2025-03-04 22:20:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function toggleForm() {
+     var formContainer = document.getElementById('formContainer');
+     // Toggle the visibility of the form
+     if (formContainer.style.display === "block") {
+         formContainer.style.display = "none";
+     } else {
+         formContainer.style.display = "block";
+     }
+ }
+ 
